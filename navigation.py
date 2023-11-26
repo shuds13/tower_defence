031d47ec06fea7446072ebe7f781499a425a4624
shudson <shudson@anl.gov>
2023-11-26 16:11:18 -0600

    Restructuring

--- navigation.py
+++ navigation.py
@@ -1,4 +1,5 @@
 import pygame
+from tower import Tower, tower_types
 
 def draw_button(surface, text, position, size):
     font = pygame.font.SysFont(None, 36)
@@ -16,3 +17,32 @@ def is_click_inside_rect(click_pos, rect):
 
 def play_button(window, window_size):
     return draw_button(window, "Play Again", (window_size[0] / 2 - 150, window_size[1] / 2 + 50), (200, 50))
+
+def draw_side_panel(surface, panel_rect, current_tower_type):
+    # Draw the background of the side panel
+    pygame.draw.rect(surface, (200, 200, 200), panel_rect)  # Light grey background
+    font = pygame.font.SysFont(None, 24)
+    y_offset = 10
+
+    def draw_tower_option(tower_type, y_offset):
+        # Draw tower selection options (simple rectangles or icons)
+        rect = pygame.Rect(panel_rect.x + 10, panel_rect.y + y_offset, 180, 50)  # Example
+        pygame.draw.rect(surface, (100, 100, 100), rect)  # Dark grey option box
+
+        image_rect_1 = tower_type.image.get_rect(center=(rect.centerx - 40, rect.centery))
+        surface.blit(tower_type.image, image_rect_1.topleft)
+        price_text_1 = font.render(f"${tower_type.price}", True, (0, 0, 0))
+        surface.blit(price_text_1, (image_rect_1.right + 5, rect.centery - 10))
+
+        if tower_type.name == current_tower_type.name:
+            pygame.draw.rect(surface, (255, 255, 0), rect, 3)  # Yellow border
+
+        return rect
+
+    #TODO make image part of tower class and use tower_type.image
+    tower_rects = []
+    for tower in tower_types:  # keep this list in tower module
+        tower_rects.append(draw_tower_option(tower, y_offset))
+        y_offset += 60
+
+    return tower_rects
--- play.py
+++ play.py
@@ -8,8 +8,6 @@ from placements import is_valid_position
 import navigation as nav
 import levels as lev
 
-
-
 initial_lives = 10
 initial_money = 100
 initial_level = 1
@@ -38,11 +36,15 @@ def reset_level():
 # Initialize Pygame
 pygame.init()
 
+pygame.display.set_caption("Tower Defense Game")
+clock = pygame.time.Clock()
+
 # Set up the display
 window_size = (900, 600)
 window = pygame.display.set_mode(window_size)
-pygame.display.set_caption("Tower Defense Game")
-clock = pygame.time.Clock()
+side_panel_width = 200
+side_panel_height = window_size[1]  # same as the game window height
+side_panel_rect = pygame.Rect(window_size[0] - side_panel_width, 0, side_panel_width, side_panel_height)
 
 # Define a simple path as a list of (x, y) tuples - will be under map.py
 path = [(50, 100), (200, 100), (200, 300), (400, 300), (400, 500), (650, 500)]
@@ -53,44 +55,9 @@ alert_message = ""
 alert_timer = 0
 restart_timer = 12000
 play_again_button = None  # To store the button rectangle
-#money_per_kill = 1
 round_bonus = 20
 
 reset_game()
-
-side_panel_width = 200
-side_panel_height = window_size[1]  # same as the game window height
-side_panel_rect = pygame.Rect(window_size[0] - side_panel_width, 0, side_panel_width, side_panel_height)
-
-# tmp here - put in placements or navigation
-def draw_side_panel(surface, panel_rect):
-    # Draw the background of the side panel
-    pygame.draw.rect(surface, (200, 200, 200), panel_rect)  # Light grey background
-    font = pygame.font.SysFont(None, 24)
-    y_offset = 10
-
-    def draw_tower_option(tower_type, y_offset):
-        # Draw tower selection options (simple rectangles or icons)
-        rect = pygame.Rect(panel_rect.x + 10, panel_rect.y + y_offset, 180, 50)  # Example
-        pygame.draw.rect(surface, (100, 100, 100), rect)  # Dark grey option box
-
-        image_rect_1 = tower_type.image.get_rect(center=(rect.centerx - 40, rect.centery))
-        surface.blit(tower_type.image, image_rect_1.topleft)
-        price_text_1 = font.render(f"${tower_type.price}", True, (0, 0, 0))
-        surface.blit(price_text_1, (image_rect_1.right + 5, rect.centery - 10))
-
-        if tower_type.name == current_tower_type.name:
-            pygame.draw.rect(surface, (255, 255, 0), rect, 3)  # Yellow border
-
-        return rect
-
-    #TODO make image part of tower class and use tower_type.image
-    tower_rects = []
-    for tower in tower_types:  # keep this list in tower module
-        tower_rects.append(draw_tower_option(tower, y_offset))
-        y_offset += 60
-
-    return tower_rects
 
 def select_tower_type(tower_types):
     for i in range(len(tower_types)):
@@ -185,7 +152,7 @@ while running:
     #window.fill((0, 0, 0))  # Clear screen
     window.fill((50, 25, 0))  # Clear screen
 
-    tower_option_rects = draw_side_panel(window, side_panel_rect)
+    tower_option_rects = nav.draw_side_panel(window, side_panel_rect, current_tower_type)
     #tower_option_rects = draw_side_panel(window, side_panel_rect, tower_img_1)
 
     if alert_timer > 0:
