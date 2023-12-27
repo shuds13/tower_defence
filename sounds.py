dfb3384721387c1059fc1e822403d70d1ec3203f
shudson <shudson@anl.gov>
2023-12-26 22:50:42 -0600

    Move sounds to sub-directory

--- sounds.py
+++ sounds.py
@@ -1,13 +1,13 @@
 import pygame
 pygame.mixer.init()
 
-snd_blop = pygame.mixer.Sound('blop.wav')
-snd_ting = pygame.mixer.Sound('ting.wav')
-snd_place = pygame.mixer.Sound('place.wav')
-snd_sell = pygame.mixer.Sound('sell.wav')
-snd_pop = pygame.mixer.Sound('pop.wav')
-snd_glue = pygame.mixer.Sound('splat.wav')
-snd_victory = pygame.mixer.Sound('fanfare.wav')
+snd_blop = pygame.mixer.Sound('sounds/blop.wav')
+snd_ting = pygame.mixer.Sound('sounds/ting.wav')
+snd_place = pygame.mixer.Sound('sounds/place.wav')
+snd_sell = pygame.mixer.Sound('sounds/sell.wav')
+snd_pop = pygame.mixer.Sound('sounds/pop.wav')
+snd_glue = pygame.mixer.Sound('sounds/splat.wav')
+snd_victory = pygame.mixer.Sound('sounds/fanfare.wav')
 
 
 sounds = {
@@ -20,23 +20,13 @@ sounds = {
     'victory':snd_victory,
     }
 
-#print("volume", pygame.mixer.Sound.get_volume())
-
-# TODO try removing mixer everywhere else - then need import sounds here and reference via some key
-# dont know why have to set volume for sounds individually rather than for mixer. But need import here
-# to avoid having to set volume on every call.
 def play(snd):
-    #print("sound volume", sounds[snd].get_volume())
     sounds[snd].play()
 
-
-# annoying cant just set mixer volume - atleast I could reduce some some than others if need to.
+# cant just set mixer volume - must do for each sound
 def set_volume(vol):
     for sound in sounds.values():
         sound.set_volume(vol)
 
 def get_volume():
     return sounds['blop'].get_volume()
-
-
-
